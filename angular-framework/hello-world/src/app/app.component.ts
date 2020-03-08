df5e04b14bf53db5657aadb359bc51d3b5f3ae6a
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-03-08 17:56:55 +0600

    Update app.component.ts

--- angular-framework/hello-world/src/app/app.component.ts
+++ angular-framework/hello-world/src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+function log(target, name, descriptor) {
+  console.log(target, name, descriptor)
+  // store the original function in
+   const original = descriptor.value
+  // original()
+  // a variable, do some manipulations with
+  // descriptor.value
+  descriptor.value = function(...args) {
+    console.log("Arguments", args, " were passed in this function")
+    const result = original(this, args)
+    console.log("The result of the function is", result)
+  }
+  // return the descriptor
+  return descriptor
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +23,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my Sample App';
+
+  @log
+  aSimpleMethod() {
+    console.log("Hey there!")
+  }
 }
