00c77fd2700e257a7b9ce44ba38a4c23e7fcff3d
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-03-08 10:47:20 +0600

    Create data-types.js

---
+++ tutorial/data-types.js
@@ -0,0 +1,16 @@
+// data types
+
+// string
+var phrase = "This is a string.";
+// numbers
+var age = 23;
+var num = 5.1;
+// boolean
+var isMale = true;
+// null
+var flaws = null;
+// undefined
+var desc = undefined;
+
+document.write(phrase);
+document.write(phrase.length);
