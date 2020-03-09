23e83048f4db4a50dc383a5d2a494d36da840255
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-03-09 12:43:20 +0600

    added code from tutorial

--- angular-framework/hello-world/src/app/app.module.ts
+++ angular-framework/hello-world/src/app/app.module.ts
@@ -9,15 +9,33 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { RecordsService } from './records.service';
 
+import { DataComponent } from './data/data.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    DataComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+
+    /* RouterModule.forRoot(
+      {
+        path: 'data/',
+        component: DataComponent
+      },
+      {
+        path: 'home/',
+        component: HomeComponent
+      }
+    )
+    */
   ],
   providers: [RecordsService],
   bootstrap: [AppComponent]
