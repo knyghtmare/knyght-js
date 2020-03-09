f7c6be71b63910fb24f89b8eb4b86748fc99be0a
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-03-09 15:38:03 +0600

    cleaned up and done tutorial 2

--- angular-framework/intro/src/app/app.component.spec.ts
+++ angular-framework/intro/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hello-world'`, () => {
+  it(`should have as title 'intro'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-world');
+    expect(app.title).toEqual('intro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hello-world app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('intro app is running!');
   });
 });
