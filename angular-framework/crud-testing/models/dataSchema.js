d846749e3a578e892f2a4819770d5180d809d9d6
knyghtmare <tahsinjahinkhalid@gmail.com>
2020-04-16 04:03:25 +0600

    removed angular stuff

--- angular-framework/crud-testing/models/dataSchema.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var countrySchema = mongoose.Schema({
-    name: {type:String},
-    capital: {stype:String}
-});
-
-module.exports = mongoose.model('country', countrySchema);
