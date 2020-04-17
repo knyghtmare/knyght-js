947d9273bcc6ea3400df1815b73df3ffccb06fd7
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2020-04-17 11:50:35 +0600

    added code from udemy crash course

---
+++ udemy/scripts/days.js
@@ -0,0 +1,31 @@
+function getDayName(day_num) {
+    var day;
+
+    switch (day_num) {
+      case "1":
+        day = "Sunday";
+        break;
+      case "2":
+        day = "Monday";
+        break;
+      case "3":
+        day = "Tuesday";
+        break;
+      case "4":
+        day = "Wednesday";
+        break;
+      case "5":
+        day = "Thursday";
+        break;
+      case "6":
+        day = "Friday";
+        break;
+      case "7":
+        day = "Saturday";
+        break;
+      default:
+        day = undefined;
+        break;
+    }
+    return day;
+}
---
+++ udemy/scripts/dogage.js
@@ -0,0 +1,3 @@
+function age_covert(age) {
+    return age*7;
+}
